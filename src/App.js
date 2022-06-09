292a898f4201b0062ac52a6628b069a286e76710
moin21 <mamoinuddin@gmail.com>
2022-06-09 11:29:01 +0530

    Added: Logo

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
