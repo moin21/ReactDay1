e3226f2aaa380e8fbcc7698edb770f3c0400ea40
moin21 <mamoinuddin@gmail.com>
2022-06-09 21:11:46 +0530

    Added: Name field to display on heading

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import logo from "./logo.svg";
+import logo from "./bl-logo.svg";
 import React from "react";
 import "./App.css";
 class App extends React.Component {
+  url = "http://www.bridgelabz.com/";
   constructor() {
     super();
     this.state = {
-      title: "Hello From Bridgelabz",
+      userName: "",
     };
   }
+
+  //onClick Function
+  onClick = ($event) => {
+    console.log("Save button is clicked !", $event);
+    window.open(this.url, "_blank");
+  };
+  onNameChange = (event) => {
+    console.log("value is", event.target.value);
+    this.setState({ userName: event.target.value });
+  };
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-    );
-  }
-  NoApp() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className="App">
+          <h1>Hello {this.state.userName} From Bridgelabz</h1>
+          <img
+            src={logo}
+            onClick={this.onClick}
+            className="App-logo"
+            alt="logo"
+          />
+        </div>
+        <div className="App">
+          <br />
+          <input onChange={this.onNameChange} />
+        </div>
+      </>
     );
   }
 }
-
 export default App;
